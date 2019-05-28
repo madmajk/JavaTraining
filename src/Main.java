8d2aebfe38debb3bc5db112b3fa1b3fcd12e68ae
madmajk <michal.dmochowski1@gmail.com>
2019-05-28 23:15:29 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+
+	public static void main(String[] args) {
+
+		int localInteger = 2000;
+		System.out.println(localInteger);
+		localInteger = 100;
+		System.out.println(localInteger);
+		
+	}
 }
